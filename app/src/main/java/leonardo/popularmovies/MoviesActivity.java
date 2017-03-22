3078f59d62d8c050dbdce1e65728afc5043e0588
LBR2048 <leo.ardjomand@gmail.com>
2017-03-22 17:15:35 -0300

    Add up navigation to MoviesActivity through abstract class MyAppCompatActivity

--- app/src/main/java/leonardo/popularmovies/MoviesActivity.java
+++ app/src/main/java/leonardo/popularmovies/MoviesActivity.java
@@ -3,10 +3,9 @@ package leonardo.popularmovies;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-public class MoviesActivity extends AppCompatActivity implements
+public class MoviesActivity extends MyAppCompatActivity implements
         MoviesFragment.OnMoviesFragmentInteractionListener,
         MovieDetailsFragment.OnFragmentInteractionListener{
 
