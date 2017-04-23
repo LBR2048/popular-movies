40a803cada36e18afc76738e430a58203ae5b0c9
LBR2048 <leo.ardjomand@gmail.com>
2017-04-23 16:49:03 -0300

    Add Content Provider class, add reference to the manifest and update DbHelper and Contract classes

--- app/src/main/java/leonardo/popularmovies/data/MoviesContract.java
+++ app/src/main/java/leonardo/popularmovies/data/MoviesContract.java
@@ -1,8 +1,23 @@
 package leonardo.popularmovies.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by unity on 19/04/17.
  */
 
 public class MoviesContract {
+
+    public static final class MoviesEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "movies";
+
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_POSTER = "poster";
+        public static final String COLUMN_OVERVIEW = "overview";
+        public static final String COLUMN_RATING = "rating";
+        public static final String COLUMN_RELEASE_DATE = "releaseDate";
+        public static final String COLUMN_FAVORITE = "favorite";
+    }
+
 }
--- app/src/main/java/leonardo/popularmovies/data/MoviesDbHelper.java
+++ app/src/main/java/leonardo/popularmovies/data/MoviesDbHelper.java
@@ -1,15 +1,41 @@
 package leonardo.popularmovies.data;
 
-import android.provider.BaseColumns;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by unity on 19/04/17.
  */
 
-public class MoviesDbHelper implements BaseColumns {
+public class MoviesDbHelper extends SQLiteOpenHelper {
 
-    public static final String TABLE_NAME = "movies";
+    public static final String DATABASE_NAME = "videos.db";
 
-    public static final String COLUMN_FAVORITE = "favorite";
+    private static final int DATABASE_VERSION = 1;
 
+    public MoviesDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_FAVORITE_MOVIES_TABLE =
+                "CREATE TABLE " + MoviesContract.MoviesEntry.TABLE_NAME + " (" +
+                        MoviesContract.MoviesEntry._ID +
+                        MoviesContract.MoviesEntry.COLUMN_POSTER +
+                        MoviesContract.MoviesEntry.COLUMN_OVERVIEW +
+                        MoviesContract.MoviesEntry.COLUMN_RATING +
+                        MoviesContract.MoviesEntry.COLUMN_RELEASE_DATE +
+                        MoviesContract.MoviesEntry.COLUMN_TITLE +
+                        ") ON CONFLICT REPLACE);";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_FAVORITE_MOVIES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + MoviesContract.MoviesEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
 }
